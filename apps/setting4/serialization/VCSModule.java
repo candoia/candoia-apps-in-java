0be49e9a45297d7ad3a1095a797ac088453e375c
nitin <nitin@iastate.edu>
2016-09-06 01:02:44 -0500

    more apps

---
+++ apps/setting4/serialization/VCSModule.java
@@ -0,0 +1,70 @@
+package setting4.serializationViolations;
+
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class VCSModule {
+	private Repository repository;
+	private Git git;
+	private String path;
+
+	public VCSModule(String path) {
+		this.path = path;
+		try {
+			this.repository = new FileRepositoryBuilder().setGitDir(new File(path + "/.git")).setMustExist(true).build();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Repository getRepository() {
+		return this.repository;
+	}
+
+	public ASTNode createAst(String fileContent) {
+		Map<String, String> options = JavaCore.getOptions();
+		options.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_5);
+		options.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_1_5);
+		options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_5);
+		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		parser.setSource(fileContent.toCharArray());
+		parser.setCompilerOptions(options);
+		ASTNode ast = parser.createAST(null);
+		return ast;
+	}
+
+	public List<String> getAllFilesFromHeadWithAbsPath() {
+		ArrayList<String> results = new ArrayList<>();
+		try {
+			Ref head = this.repository.getRef("HEAD");
+			RevWalk walk = new RevWalk(this.repository);
+			RevCommit revision = walk.parseCommit(head.getObjectId());
+			RevTree tree = revision.getTree();
+			TreeWalk walker = new TreeWalk(this.repository);
+			walker.addTree(tree);
+			walker.setRecursive(true);
+			while (walker.next()) {
+				results.add(this.path + "/" + walker.getPathString());
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return results;
+	}
+}
