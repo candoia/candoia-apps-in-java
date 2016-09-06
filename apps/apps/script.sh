f0fe1f144681d02d23603d60624fc3722a0b14cc
Nitin Tiwari <nmtiwari@iastate.edu>
2016-09-06 13:52:20 -0500

    Adding two more apps

--- apps/apps/script.sh
+++
@@ -1,7 +0,0 @@
-
-for f in setting1/bugFileMapper/*; do
-  added=`diff -u -s  $f setting2/bugFileMapper/ | grep ^+ | wc -l`
-  removed=`diff -u -s  $f setting2/bugFileMapper/ | grep ^- | wc -l`
-  echo $f  $added $removed 
-done
-
